5dfc77d76dc95cd5a807a21d044d632d0280d01c
Joao Pedro <dp.joaopedro@gmail.com>
2020-05-26 20:17:40 -0300

    added budget screen

--- lib/screens/budget_screen.dart
+++ lib/screens/budget_screen.dart
@@ -1,13 +1,20 @@
+import 'package:empreiteiraApp/components/app_drawer.dart';
 import 'package:flutter/material.dart';
 
 class BudgetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 2,
-      itemBuilder: (context, index) {
-        return Text('Implementing');
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Orçamentos'),
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return Text('Implementing');
+        },
+      ),
     );
   }
 }
