c4515207d2035795bd66da4d71d120b021935f35
Joao Pedro <dp.joaopedro@gmail.com>
2020-05-28 19:28:37 -0300

    adjust dummy data

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -7,7 +7,7 @@ import 'package:empreiteiraApp/models/payment_item_model.dart';
 final dummyData = [
   BudgetModel(
     id: '1',
-    title: 'Orçamento 1',
+    title: 'Orçamento Modelo',
     price: 1500,
     materialsAndTools: 'Todos os materiais e ferramentas são de responsabilidade do contratante',
     client: ClientModel(id: '1', cod: '100.899.946-66', name: 'Funalo da Silva'),
